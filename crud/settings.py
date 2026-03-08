ccb4780c277c7821311695db4bdd9f1ca7c89964
rajesh827 <rajeshbhandari2075@gmail.com>
2026-03-08 20:57:17 +0545

    update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -62,7 +62,8 @@ ROOT_URLCONF = 'crud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend' / 'dist'],
+        # 'DIRS': [BASE_DIR / 'frontend' / 'dist'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'dist' / 'assets']
+# STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'dist' / 'assets']
+STATICFILES_DIRS = []
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
