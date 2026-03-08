0c0b0430372d2d184109ff6245eca39f85cc8ac8
rajesh827 <rajeshbhandari2075@gmail.com>
2026-03-08 21:10:40 +0545

    Fix URLs for API

--- crud/urls.py
+++ crud/urls.py
@@ -1,29 +1,25 @@
-"""
-URL configuration for crud project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+# This creates a simple message when you visit the root URL so you know it works
+def api_home(request):
+    return JsonResponse({
+        "status": "success",
+        "message": "Django Backend API is running perfectly!"
+    })
 
 urlpatterns = [
+    # Admin Panel
     path('admin/', admin.site.urls),
+    
+    # Your Grocery API Routes
     path('api/grocery/', include('grocery.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='react_app'),
+    
+    # The new Home URL
+    path('', api_home, name='api_home'),
 ]
 
 # Serve media files during development
